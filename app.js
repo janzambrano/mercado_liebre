bef2dc3afa66628bd6b656c72218cb2ed854ada1
janzambrano <janczambranol@gmail.com>
2022-05-20 10:01:08 -0400

    actualizacion general y cambios de ejercicios de formularios

--- app.js
+++ app.js
@@ -9,3 +9,9 @@ app.use ( express.static( path.join(__dirname, './public')));
 
 app.get('/', (req, res) => { res.sendFile(path.join(__dirname, '/views/home.html')); 
 });
+
+app.get('/register', (req, res) => { res.sendFile(path.join(__dirname, '/views/register.html')); 
+});
+
+app.get('/login', (req, res) => { res.sendFile(path.join(__dirname, '/views/login.html')); 
+});
