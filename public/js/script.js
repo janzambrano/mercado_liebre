4a09fe5c8fa49c3961ba616a89eebede8c2cf5ec
janzambrano <janczambranol@gmail.com>
2022-05-20 16:14:21 -0400

    agregado validacion doble de contraseña2

--- public/js/script.js
+++ public/js/script.js
@@ -3,20 +3,7 @@ function validate(){
     var pass = document.getElementById('contrasena1').value
     var pass2 = document.getElementById('contrasena2').value
     
-   if(!email.match(emailRegex)) {
-        alert("Por forvor ingrese un email valido.");
-        return false;
-    }
-    else if(email == "") {
-        alert("Por favor inserte su email.");
-        return false;
-    }
-
-    else if(pass.length==0){
-        alert("Por favor inserte su contraseña.");
-        return false;
-    }
-    else if(pass!=pass2){
+   if(pass!=pass2){
         alert("Las contraseñas no coinciden. Inténtalo de nuevo.");
         return false;
     }
