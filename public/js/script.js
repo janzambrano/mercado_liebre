2bec00bf1a91217650b2e45e15e1ed63c642d13c
janzambrano <janczambranol@gmail.com>
2022-05-20 21:29:57 -0400

    actualizacion en script.js

--- public/js/script.js
+++ public/js/script.js
@@ -3,7 +3,7 @@ function validate(){
     var pass = document.getElementById('contrasena1').value
     var pass2 = document.getElementById('contrasena2').value
     
-   if(pass!=pass2){
+    if(pass!=pass2){
         alert("Las contraseñas no coinciden. Inténtalo de nuevo.");
         return false;
     }
