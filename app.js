9b8c133fef70295ae770ffa55831a4c1be70586b
janzambrano <janczambranol@gmail.com>
2022-05-26 21:28:15 -0400

    agregando puerto para Heroku

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.listen(3000, () => console.log('Levantando un servidor con Express') 
+const port = process.env.POST || 3000; // esto es para conectar con Heroku
+
+app.listen(port, () => console.log('Levantando un servidor con Express') 
 ) ; 
 
 app.use ( express.static( path.join(__dirname, './public')));
